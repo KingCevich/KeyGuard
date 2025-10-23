d4755ae5844ebc8a3c51d13842e2e22ddec7df01
NACV <na.cifva7@gmail.comm>
2025-10-23 00:22:32 -0300

    funcionamiento de dependencias y creacion de packajes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,9 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.biometric:biometric:1.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
