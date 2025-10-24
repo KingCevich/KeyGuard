a05448df269bf7dcef2959fb0ed34906695f49fd
Ismael Rivas <isma.rivas@duocuc.cl>
2025-10-23 21:34:27 -0300

    Pantalla Homescreen

--- app/src/main/java/com/example/keyguard/ui/HomePage.kt
+++ app/src/main/java/com/example/keyguard/ui/HomePage.kt
@@ -1,2 +1,53 @@
 package com.example.keyguard.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.keyguard.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    Scaffold (
+        topBar = {
+            TopAppBar(title = { Text("Keyguard") })
+        }
+    ) {innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Text(text = "¡Bienvenido!")
+            Button(onClick = { /* acción futura*/ } ) {
+                Text("Presioname")
+            }
+            Image(
+                painter = painterResource(id = R.drawable.keyguardtest),
+                contentDescription = "Logo app",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
